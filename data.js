297a4169ae47925b357268380abcc4bc87bb41be
Never-hate <kbro2503@gmail.com>
2026-02-22 09:08:02 +0600

    Add prayer data for Jamalpur Namaz Time

---
+++ data.js
@@ -0,0 +1,18 @@
+const PRAYER_DATA = {
+    appName: "JAMALPUR NAMAZ TIME",
+    location: "Jamalpur, Bangladesh",
+    hijriDay: "21",
+    hijriMonth: "Rajab",
+    hijriYear: "1443 Hijri",
+    sehri: "5:14 AM",
+    iftar: "5:57 PM",
+    nextPrayer: "Dhuhr: 3h 19m left",
+    times: [
+        { name: "Fajr", time: "5:24 AM", icon: "🌅" },
+        { name: "Dhuhr", time: "12:12 PM", icon: "☀️" },
+        { name: "Asr", time: "3:29 PM", icon: "🌤️" },
+        { name: "Maghrib", time: "5:57 PM", icon: "🌆" },
+        { name: "Isha", time: "7:00 PM", icon: "🌙" },
+        { name: "Sunrise", time: "6:27 AM", icon: "🔆" }
+    ]
+};
